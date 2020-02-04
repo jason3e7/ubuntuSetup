cd62e31a8b0e96446175d30711d77550c2652006
jason3e7 <jason3e7@gmail.com>
2020-02-04 10:26:39 +0800

    add elastic gui

---
+++ Dejavu.sh
@@ -0,0 +1,6 @@
+# ref https://github.com/appbaseio/dejavu
+
+docker run -p 1358:1358 -d appbaseio/dejavu
+
+# open http://localhost:1358/
+# enable CORS settings
---
+++ ElasticHQ.sh
@@ -0,0 +1,9 @@
+# ref https://dev.to/hsatac/management-gui-for-elasticsearch-17g9
+
+git clone https://github.com/ElasticHQ/elasticsearch-HQ.git
+cd elasticsearch-HQ
+sudo pip3 install -r requirements.txt
+# sudo apt install python3-pip
+python3 application.py
+
+# Access HQ with: http://localhost:5000
